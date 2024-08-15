0bf6a5b60f2e1067a6022153e7ea0eaca927dc2e
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2024-08-15 14:21:50 +0200

    Add basic qdarnt vectorsearch without user interaction

--- python/main.py
+++ python/main.py
@@ -1,63 +1,43 @@
-import chromadb
-from chromadb.config import Settings
-import time
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams, PointStruct
 
-print("Wait 10 second for ChromaDB instantiate")
-time.sleep(10)
+client = QdrantClient(url="http://qdrant:6333")
+COLLECTION_NAME = "test_collection"
 
-chroma_client = chromadb.HttpClient(host="chroma", port = 8000, settings=Settings(allow_reset=True, anonymized_telemetry=False))
-documents = [
-    "Mars, often called the 'Red Planet', has captured the imagination of scientists and space enthusiasts alike.",
-    "The Hubble Space Telescope has provided us with breathtaking images of distant galaxies and nebulae.",
-    "The concept of a black hole, where gravity is so strong that nothing can escape it, was first theorized by Albert Einstein's theory of general relativity.",
-    "The Renaissance was a pivotal period in history that saw a flourishing of art, science, and culture in Europe.",
-    "The Industrial Revolution marked a significant shift in human society, leading to urbanization and technological advancements.",
-    "The ancient city of Rome was once the center of a powerful empire that spanned across three continents.",
-    "Dolphins are known for their high intelligence and social behavior, often displaying playful interactions with humans.",
-    "The chameleon is a remarkable creature that can change its skin color to blend into its surroundings or communicate with other chameleons.",
-    "The migration of monarch butterflies spans thousands of miles and involves multiple generations to complete.",
-    "Christopher Nolan's 'Inception' is a mind-bending movie that explores the boundaries of reality and dreams.",
-    "The 'Lord of the Rings' trilogy, directed by Peter Jackson, brought J.R.R. Tolkien's epic fantasy world to life on the big screen.",
-    "Pixar's 'Toy Story' was the first feature-length film entirely animated using computer-generated imagery (CGI).",
-    "Superman, known for his incredible strength and ability to fly, is one of the most iconic superheroes in comic book history.",
-    "Black Widow, portrayed by Scarlett Johansson, is a skilled spy and assassin in the Marvel Cinematic Universe.",
-    "The character of Iron Man, played by Robert Downey Jr., kickstarted the immensely successful Marvel movie franchise in 2008."
-]
-metadatas = [{'source': "Space"}, {'source': "Space"}, {'source': "Space"}, {'source': "History"}, {'source': "History"}, {'source': "History"}, {'source': "Animals"}, {'source': "Animals"}, {'source': "Animals"}, {'source': "Movies"}, {'source': "Movies"}, {'source': "Movies"}, {'source': "Superheroes"}, {'source': "Superheroes"}, {'source': "Superheroes"}]
-ids = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"]
+def create_collection() -> None:
+    client.create_collection(
+        collection_name=COLLECTION_NAME,
+        vectors_config=VectorParams(size=4, distance=Distance.DOT)
+    )
 
-collection_status = False
-while collection_status != True:
-    try:
-        document_collection = chroma_client.get_or_create_collection(name="sample_collection")
-        collection_status = True
-    except Exception as e:
-        pass
+def upser_sample_data_into_qdrant() -> None:
+    client.upsert(
+    collection_name=COLLECTION_NAME,
+    wait=True,
+    points=[
+        PointStruct(id=1, vector=[0.05, 0.61, 0.76, 0.74], payload={"city": "Berlin"}),
+        PointStruct(id=2, vector=[0.19, 0.81, 0.75, 0.11], payload={"city": "London"}),
+        PointStruct(id=3, vector=[0.36, 0.55, 0.47, 0.94], payload={"city": "Moscow"}),
+        PointStruct(id=4, vector=[0.18, 0.01, 0.85, 0.80], payload={"city": "New York"}),
+        PointStruct(id=5, vector=[0.24, 0.18, 0.22, 0.44], payload={"city": "Beijing"}),
+        PointStruct(id=6, vector=[0.35, 0.08, 0.11, 0.44], payload={"city": "Mumbai"}),
+    ],
+)
 
-document_collection.add(documents=documents, metadatas=metadatas, ids=ids)
+def check_if_collection_exists() -> bool:
+    collections = client.get_collections()
+    return COLLECTION_NAME in [collection.name for collection in collections.collections]
 
-firstTimeOpened = True
+def main() -> None:
+    if not check_if_collection_exists():
+        create_collection()
+        upser_sample_data_into_qdrant()
 
-while(True):
+    search_result = client.search(
+        collection_name=COLLECTION_NAME, query_vector=[0.19, 0.81, 0.75, 0.11], limit=1
+    )
 
-    if firstTimeOpened != False:
-        print('Wait 5 seconds for connection to ChromaDB...')
-        time.sleep(5)
-        firstTimeOpened = False
+    print(search_result)
 
-    query = input("About what would you like to watch a movie?: ")
-    results = document_collection.query(query_texts=query , n_results=3)
-
-    result_documents = results["documents"][0]
-    for doc in result_documents:
-        print(doc)
-
-    keepAsking = input("Do you have another question?[Y/n]: ")
-
-    if keepAsking == 'Y' :
-        continue
-    elif keepAsking == 'n' :
-        print('See you again!')
-        break
-    else :
-        continue
+if __name__ == '__main__':
+    main()
